80f3fb043eaad1c17613723cc79766aede82d9e3
zappy586 <mohdzeesh2002@gmail.com>
2026-06-10 11:45:36 +0530

    initial commit

--- q3 kth largest.py
+++ q3 kth largest.py
@@ -1,17 +1,17 @@
 def sort_arr(nums, k):
     n = len(nums)
     temp = 0
-    i, j = 0, 1
-    while j < n:
+    i, j = 0, n - 1
+    while i < j:
         print(nums)
         if nums[i] > nums[j]:
             temp = nums[i]
             nums[i] = nums[j]
             nums[j] = temp
-            j += 1
-        else:
             i += 1
+        else:
+            j -= 1
     return nums[k]
 
-nums = [3, 2, 1, 5, 6, 4]
+nums = [3, 2, 1, 5, 6, 4, 42, 0]
 print(sort_arr(nums, 2))
